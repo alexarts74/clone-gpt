5b5e26f0f23da3e62634fbf2cf50c53edebb20fa
alexandre artus <artusalexandre@yahoo.fr>
2024-01-21 18:24:28 +0100

    Ajout du fichier .gitignore et exclusion du fichier .env

--- app.py
+++ app.py
@@ -1,10 +1,51 @@
-from flask import Flask, render_template
+import os
+import openai
+from dotenv import load_dotenv
+from flask import Flask, render_template, request, Response
+
+load_dotenv()
+
+openai.api_key = os.getenv("KEY_OPEN_AI")
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
     return render_template('index.html')
 
+
+
+@app.route("/prompt", methods=["POST"])
+def prompt():
+    messages = request.json["messages"]
+    conversation = build_conversation_dict(messages)
+
+    return Response(event_stream(conversation), mimetype="text/event-sream")
+
+
+
+def build_conversation_dict(messages: list) -> list[dict]:
+    return [
+        {"role": "user" if i % 2 == 0 else "assistant", "content": message}
+        for i, message in enumerate(messages)
+    ]
+
+
+def event_stream(conversation: list[dict]) -> str:
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=conversation,
+        stream=True
+    )
+
+    for line in response:
+        text = line.choices[0].delta.get('content', '')
+        if len(text):
+            yield text
+
+
 if __name__ == '__main__':
     app.run(debug=True, host='127.0.0.1', port=5000)
+    conversation = build_conversation_dict(messages=["bonjour", "hola"])
+    print(event_stream(conversation))
--- static/dist/js/script.js
+++ static/dist/js/script.js
@@ -1,1 +1,83 @@
+function cloneAnswerBlock() {
+  const output = document.querySelector("#gpt-output");
+  const template = document.querySelector('#chat-template');
+  const clone = template.cloneNode(true);
+  clone.id = "";
+  output.appendChild(clone);
+  clone.classList.remove("hidden")
+  return clone.querySelector(".message");
+}
 
+
+function addToLog(message) {
+  let answerBlock = cloneAnswerBlock();
+  answerBlock.textContent = message;
+  return answerBlock
+}
+
+
+
+function getChatHistory() {
+  const messageBlocks = document.querySelectorAll(".message:not(#chat-template .message)")
+  return Array.from(messageBlocks).map(block => block.innerHTML)
+}
+
+
+
+async function fetchPromptResponse() {
+  const response = await fetch("/prompt", { //url de la route en python
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json",
+      },
+      body: JSON.stringify({messages: getChatHistory()}),
+  });
+
+  return response.body.getReader();
+}
+
+
+async function readResponseChunks(reader, gptanswer) {
+  const decoder = new TextDecoder();
+  const converter = new showdown.Converter();
+
+  let chunks = "";
+  while (true) {
+      const {done, value} = await reader.read();
+      if (done) {
+          break;
+      }
+      chunks += decoder.decode(value);
+      gptanswer.innerHTML = converter.makeHtml(chunks);
+  }
+}
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector("#prompt-form");
+  const spinnerIcon = document.querySelector("#spinner-icon");
+  const sendIcon = document.querySelector("#send-icon");
+
+  form.addEventListener("submit", async (event) => {
+      event.preventDefault(); //Empeche l'envoie du formulaire
+      spinnerIcon.classList.remove("hidden");
+      sendIcon.classList.add("hidden");
+
+      const prompt = form.elements.prompt.value; //recup la valeur de l'input
+      addToLog(prompt); //ajoute les messages au block
+      form.elements.prompt.value = ""
+
+      try {
+          const gptOutput = addToLog("GPT est en train de réfléchir...");
+          const reader = await fetchPromptResponse(prompt);
+          await readResponseChunks(reader, gptOutput);
+      } catch (error) {
+          console.error('Une erreur est survenue:', error);
+      } finally {
+          spinnerIcon.classList.add("hidden");
+          sendIcon.classList.remove("hidden");
+          hljs.highlightAll();
+      }
+  });
+});
